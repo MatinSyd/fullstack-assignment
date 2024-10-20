d8ea1cda36993d14f9e2c822d4e2a8aa028f58ef
maateen <matin@matin>
2024-10-20 20:17:15 +0530

    settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -85,13 +85,9 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',  
-        'NAME': 'fullstack',  
-        'USER': 'fullstack',  
-        'PASSWORD': 'fullstack',  
-        'HOST': 'localhost',  
-        'PORT': '5432',  
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
